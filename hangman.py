bb81a9817031c5e0c330089a75cc109d73b070ec
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-05 20:25:23 +0100

    Hidden letters should be replaced with hyphens ("-").

--- hangman.py
+++ hangman.py
@@ -1,2 +1,22 @@
+import random
+
 print("H A N G M A N")
-print("The game will be available soon.")
+
+prog_lang = ['python', 'java', 'kotlin', 'javascript']
+
+guess_word = random.choice(prog_lang)
+
+first_3 = guess_word[:3]
+
+last_n = guess_word[3:len(guess_word):1]
+
+fill_word = "-" * (len(guess_word) - 3)
+
+print(f"Guess the word {first_3}{fill_word}: >")
+
+possible_word = input()
+
+if guess_word == possible_word:
+    print("You survived!")
+else:
+    print("You lost!")
