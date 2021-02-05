f6efa11231c5df21429cb08ace7b8732858ab7e9
Chukwuka1488 <juniorakibor@yahoo.com>
2021-02-05 20:35:41 +0100

    computer choice is hidden

--- hangman.py
+++ hangman.py
@@ -9,3 +9,7 @@ prog_lang = ['python', 'java', 'kotlin', 'javascript']
 
 # computer chooses from word
 guess_word = random.choice(prog_lang)
+
+# computer choice is hidden
+empty_word = "-" * (len(guess_word))
+print(empty_word)
